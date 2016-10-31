b45e7a992443af963ff2588afa77bc3f48e8adf1
Mordin <azet89@gmail.com>
2016-10-31 18:24:58 +0100

    Initial Commit

---
+++ src/main/java/com/mprtcz/TicTacToeUltimateApplication.java
@@ -0,0 +1,12 @@
+package com.mprtcz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeUltimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeUltimateApplication.class, args);
+	}
+}
