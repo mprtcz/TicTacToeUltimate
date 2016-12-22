c817ba604a12c77be6d562a9b331ed55cf506605
mprtcz <azet89@gmail.com>
2016-12-22 13:53:20 +0100

    Added mapstruct configuration which allows for mapping a list of game maves to game moveDTOs stored in a collection inside GameRecord class;

--- src/main/java/com/mprtcz/tictactoeultimate/game/model/mapper/GameMoveMapper.java
+++ src/main/java/com/mprtcz/tictactoeultimate/game/model/mapper/GameMoveMapper.java
@@ -4,6 +4,8 @@ import com.mprtcz.tictactoeultimate.game.model.GameMove;
 import com.mprtcz.tictactoeultimate.game.model.dto.GameMoveDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 /**
  * Created by mprtcz on 2016-11-03.
  */
@@ -13,4 +15,6 @@ public interface GameMoveMapper {
     GameMoveDTO toDTO(GameMove gameMove);
 
     GameMove toEntity(GameMoveDTO gameMoveDTO);
+
+    List<GameMoveDTO> toMoveDTOs(List<GameMove> gameMove);
 }
--- src/main/java/com/mprtcz/tictactoeultimate/game/model/mapper/GameRecordMapper.java
+++ src/main/java/com/mprtcz/tictactoeultimate/game/model/mapper/GameRecordMapper.java
@@ -4,8 +4,7 @@ import com.mprtcz.tictactoeultimate.game.model.GameRecord;
 import com.mprtcz.tictactoeultimate.game.model.dto.GameRecordDTO;
 import com.mprtcz.tictactoeultimate.user.model.mapper.UserMapper;
 import org.mapstruct.Mapper;
-
-import java.util.List;
+import org.mapstruct.Mapping;
 
 /**
  * Created by mprtcz on 2016-11-03.
@@ -13,9 +12,8 @@ import java.util.List;
 @Mapper(componentModel = "spring", uses = {UserMapper.class, GameMoveMapper.class})
 public interface GameRecordMapper {
 
+    @Mapping(source = "gameRecord.movesList", target = "gameMovesList")
     GameRecordDTO toDTO(GameRecord gameRecord);
 
     GameRecord toEntity(GameRecordDTO gameRecordDTO);
-
-    List<GameRecordDTO> toDTOs(List<GameRecord> gameRecords);
 }
