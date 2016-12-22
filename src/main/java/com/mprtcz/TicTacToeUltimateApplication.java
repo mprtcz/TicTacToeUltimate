0afa38850b9541ae84a73f50f6f430258fdc6e92
mprtcz <azet89@gmail.com>
2016-12-22 10:48:51 +0100

    Minor refactoring;

--- src/main/java/com/mprtcz/TicTacToeUltimateApplication.java
+++ src/main/java/com/mprtcz/TicTacToeUltimateApplication.java
@@ -2,15 +2,12 @@ package com.mprtcz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-@EntityScan(
-		basePackageClasses = { TicTacToeUltimateApplication.class, Jsr310JpaConverters.class }
-)
+@EntityScan(basePackageClasses = { TicTacToeUltimateApplication.class, Jsr310JpaConverters.class })
 @SpringBootApplication
 public class TicTacToeUltimateApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TicTacToeUltimateApplication.class, args);
 	}
